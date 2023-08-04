f4900808443d944c5dfac9fb4b812a2969400a4c
kawahitohiroyuki <gebara39@gmail.com>
2023-08-04 02:07:00 +0000

    issue2-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # アプリケーションのルーティングをDSL形式で定義します。
+  # 詳細なルーティングの記述方法は https://guides.rubyonrails.org/routing.html を参照してください。
 
-  # Defines the root path route ("/")
+  # ルートパスを定義します ("/")
   # root "articles#index"
+
+  # PostsController
+  # "posts/new" へのアクセスを "posts#new" アクションに割り当てます
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  
+  # TopicsController
+  # "topics/new" へのアクセスを "topics#new" アクションに割り当てます
+  get 'topics/new', to: 'topics#new'
+  # "topics/edit" へのアクセスを "topics#edit" アクションに割り当てます
+  get 'topics/edit', to: 'topics#edit'
 end
